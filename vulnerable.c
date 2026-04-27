a68d1a584851b6bbc61980298024350545c8b09e
Deliya_comp <manchedelia06@gmail.com>
2026-04-27 20:00:10 +0300

    feat: add gets, sprintf, scanf examples for semgrep rules

--- vulnerable.c
+++ vulnerable.c
@@ -3,12 +3,41 @@
 
 void vulnerable_function(char *input) {
     char buffer[10];
-    strcpy(buffer, input);  // БАГ. Нет проверки длины
+    // ОПАСНО: strcpy без проверки длины
+    strcpy(buffer, input);
     printf("Copied: %s\n", buffer);
 }
 
+void test_gets() {
+    char buffer[10];
+    printf("Enter something: ");
+    // ОПАСНО: gets() не проверяет длину ввода
+    gets(buffer);
+    printf("You entered: %s\n", buffer);
+}
+
+void test_sprintf() {
+    char buffer[10];
+    int num = 12345;
+    // ОПАСНО: sprintf может переполнить буфер
+    sprintf(buffer, "Number: %d", num);
+    printf("Formatted: %s\n", buffer);
+}
+
+void test_scanf() {
+    char buffer[10];
+    printf("Enter a word: ");
+    // ОПАСНО: scanf с %s без ограничения длины
+    scanf("%s", buffer);
+    printf("You entered: %s\n", buffer);
+}
+
 int main(int argc, char **argv) {
-    if (argc < 2) return 1;
-    vulnerable_function(argv[1]);
+    if (argc > 1) {
+        vulnerable_function(argv[1]);
+    }
+    test_gets();
+    test_sprintf();
+    test_scanf();
     return 0;
 }
